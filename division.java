353ac7a0398fa2fb9683f296a532536ae0523abe
21A91A0484 <98581531+21A91A0484@users.noreply.github.com>
2022-12-19 20:25:41 +0530

    Create division.java

---
+++ division.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Demo
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        int a;
+        n=sc.nextInt();
+        a=sc.nextInt();
+        System.out.println(n/a);
+    }
+}
